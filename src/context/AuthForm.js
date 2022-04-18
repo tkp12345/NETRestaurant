ed819a8a2ed39a618fe694c90f8a4f548df6d317
tkp12345 <tkp1@hanmail.net>
2022-04-19 07:28:16 +0900

    feat : auth - conetextApi 설정 [ 로그인,회원가입,로그아웃]

---
+++ src/context/AuthForm.js
@@ -0,0 +1,62 @@
+import {React,useMemo,useState,createRef,createContext,useImperativeHandle,useCallback} from 'react';
+import { Children } from 'react/cjs/react.production.min';
+import Login from '../layout/Login';
+
+
+//context api 사용법 : https://kyounghwan01.github.io/blog/React/react-context-api/#api
+const AuthContext = createContext({});
+
+//createRef 훅 사용법 : https://velog.io/@hoon_dev/React-Ref-%EC%9D%B4%EC%9A%A9%ED%95%98%EA%B8%B0feat-createRef
+const contextRef = createRef();
+
+
+const AuthForm = () => {
+const [isLogin,setIsLogin] = useState(false);
+
+const onSignUp =useCallback(async (username,password,name,email)=>{
+ 
+});
+
+const onLogin =useCallback(async (username,password,name,email)=>{
+ 
+});
+
+const onLogout =useCallback(async (username,password,name,email)=>{
+ 
+});
+
+
+//contextApi 를 사용하기위한 상위 선언변수들을 넣어줍니다 
+const contextForm = useMemo(()=>({
+    isLogin,
+    onSignUp,
+    onLogin,
+    onLogout,
+}),[isLogin,
+    onSignUp,
+    onLogin,
+    onLogout]);
+
+
+  //자식에있는 요소를 부모에서 호출하기 위한 훅 :https://developer-alle.tistory.com/372
+  useImperativeHandle(contextRef, () => (isLogin ? isLogin.token : false));
+
+
+
+    return (
+        <AuthContext.Provider value={contextForm}>
+            {isLogin?(
+                //하위 요소 호출 
+                Children
+            ):(
+                <div>
+                    <Login onSignUp={onSignUp} onLogin={onLogin}/>
+                </div>
+            )}
+
+
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthForm;
--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import AuthForm from './context/AuthForm';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <App />
-    </BrowserRouter>,
+   <AuthForm>
+     <App />
+   </AuthForm>
+  </BrowserRouter>,
   document.getElementById(root)
 );
 
