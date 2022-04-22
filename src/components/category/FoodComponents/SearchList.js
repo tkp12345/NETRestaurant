8464ebcee43185d4b2057e293eeaacf7a09765f9
chjysm <chjysm2@naver.com>
2022-04-22 18:35:17 +0900

    test

---
+++ src/components/category/FoodComponents/SearchList.js
@@ -0,0 +1,19 @@
+import React, {useEffect} from 'react';
+
+const SearchList = () => {
+
+    useEffect(() => {
+        
+    }, []);
+
+
+    return (
+
+        <div id='myMap' style={{
+            width: '1000px',
+            height: '1000px'
+        }}></div>
+    );
+};
+
+export default Food;
